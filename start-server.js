9fd63ef4a1fb5dd59ebb4c3a6b5ec464012ba4b6
jespyownerrr <jespy@eugeniaschool.com>
2025-12-18 15:14:12 +0100

    finito

--- start-server.js
+++ start-server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const publicDir = path.join(__dirname, 'Portfolio Google Sheet');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const mimeTypes = {
   '.html': 'text/html; charset=utf-8',
